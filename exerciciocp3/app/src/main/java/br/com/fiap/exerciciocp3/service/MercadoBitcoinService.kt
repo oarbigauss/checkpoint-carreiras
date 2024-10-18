8955fa5ee2183f45368b620faa27729a81dcf998
Gabriel Arbigaus <gabriel.arbigaus@stratesys-ts.com>
2024-10-17 22:05:07 -0300

    Implementando chamada da api e finalizando projeto

---
+++ exerciciocp3/app/src/main/java/br/com/fiap/exerciciocp3/service/MercadoBitcoinService.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.exerciciocp3.service
+
+import br.com.fiap.exerciciocp3.model.TickerResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MercadoBitcoinService {
+
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
---
+++ exerciciocp3/app/src/main/java/br/com/fiap/exerciciocp3/service/MercadoBitcoinServiceFactory.kt
@@ -0,0 +1,14 @@
+package br.com.fiap.exerciciocp3.service
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MercadoBitcoinServiceFactory {
+    fun create(): MercadoBitcoinService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.mercadobitcoin.net/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(MercadoBitcoinService::class.java)
+    }
+}
